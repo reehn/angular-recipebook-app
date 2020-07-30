2a1b7225515df36d81d15e14b5498ffd08749ce5
Golaj <ch.rehn90@gmail.com>
2020-07-30 12:14:55 +0200

    Added modules, components and routings

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {RecipeService} from '../recipe.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+
+  recipe: Recipe;
+  id: number;
+
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
+
+  addToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(["edit"], {relativeTo: this.route});
+    //this.router.navigate(["../", this.id, "edit"], {relativeTo: this.route})
+  }
+
+}
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,46 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shoppling-list.service';
+
+@Injectable()
+export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Tasty Schnitzel', 'A super tasy Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger', 'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    /*ingredients.forEach(ingredient => {
+      this.shoppingListService.addIngredient(ingredient);
+    });*/
+
+    this.shoppingListService.addMultipleIngredients(ingredients);
+    alert('Ingredients added to shoppinglist!');
+  }
+
+}
